a92025abd68024ff19d3b5437ed7635928b1bf65
Matt Spell <matthew.spell1@gmail.com>
2014-08-18 17:32:09 -0500

    updated unit tests for DRY

--- spec/models/actual_spec.rb
+++ spec/models/actual_spec.rb
@@ -3,11 +3,18 @@ RSpec.describe Actual do
   it { should validate_presence_of :name }
   it { should validate_presence_of :amount }
 
-  context ".subtract_expenditure" do
-    let!(:output){ run_budget_with_input("2000", "y", "Food", "100", "y", "1", "Maffiaoza's", "20") }
-    it "should subtract the actual from the bank balance" do
-      expect(Bank.last.balance).to eql 198000
+  context ".convert_to_cents" do
+    let!(:cents){ Actual.convert_to_cents(20) }
+    it "should convert the dollar amount to change" do
+      expect(cents).to eql 2000
     end
-
   end
+
+  context ".convert_to_dollars" do
+    let!(:dollars){ Actual.convert_to_dollars(1000) }
+    it "should convert the change amount to dollars" do
+      expect(dollars).to eql "10.00"
+    end
+  end
+
 end
